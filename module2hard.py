fac14ed90ff4bfca2293e7a83b8032e91ac9b8ad
VlasyaPlay <alexvlasov9301@gmail.com>
2024-07-12 19:06:10 +0700

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -8,7 +8,7 @@ else:
 
 for i in range(1, n):
     for j in range(i + 1, n):
-        if (i + j) % n == 0:
+        if n % (i + j) == 0:
             password += str(i)
             password += str(j)
 
